03d52274c79ce5537691fddd748f99c9b4747eee
Alexander Danilenko <alexander@danilenko.org>
2012-06-30 01:28:26 +0400

    Adopt twitter bootstrap for layout

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,2 +1,12 @@
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Blogging
+
+def stylesheet_link_tag(*names)
+  names.map do |name|
+    %{<link rel="stylesheet" type="text/css" href="/stylesheets/#{name}.css" media="screen" />}
+  end.join("\n")
+end
+
+def top_nav_item(name, path)
+  "<li#{' class="active"' if @item.path == path}>#{link_to(name, path)}</li>"
+end
